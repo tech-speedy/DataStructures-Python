2945624b9277214b5e7ae17c34fe14e1278721f5
Arnab Sarkar <sarkararnab088@gmail.com>
2025-08-14 15:00:03 +0530

    Created Linked_Lists-03.py

--- Linked_Lists-03.py
+++ Linked_Lists-03.py
@@ -1,4 +1,17 @@
+#Insertion at the beginning of Linked list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
+#Creating the nodes
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+#Connecting nodes to form a Linked List
 node1.next = node2
 node2.next = node3
 node3.next = node4
